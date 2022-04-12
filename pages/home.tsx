464c470d97341de1409bd3e603142f9071008edc
Rassu1Nurdylda <201328@astanait.edu.kz>
2022-04-13 00:14:15 +0600

    third section

--- pages/home.tsx
+++ pages/home.tsx
@@ -25,6 +25,36 @@ const home: NextPage = () => {
 						</div>
 					</div>
 				</div>
+
+				<div className='container px-5 mx-auto py-24 grid gird-cols-1 justify-center justify-items-center xl:grid-cols-2'>
+					<div className='flex items-center w-4/6 md:w-8/6'>
+						<img className='rounded object-cover w-full' src="/images/anel.png" alt='anel letaet' />
+					</div>
+					<div className='w-8/12 mt-20'>
+						<div className=''>
+							<h1 className="font-semibold text-xl sm:text-4xl md:text-4xl">Track your progress</h1>
+							<div className='w-10/12 mt-5'>
+								<p className='text-base sm:text-xl'>Learn new meaning every day and track your proggress bar in profile.</p>
+							</div>
+
+							<h1 className="font-semibold text-xl sm:text-4xl md:text-4xl mt-8">Remember</h1>
+							<div className='w-10/12 mt-5'>
+								<p className='text-base sm:text-xl'>Sign up and save your favourite words and check the learned ones.</p>
+							</div>
+						</div>
+						<div>
+							<button
+								type="submit"
+								className="flex w-full cursor-pointer justify-center rounded-md bg-dark-blue p-4 
+									font-semibold tracking-wide text-gray-100 transition duration-300 ease-in-out hover:shadow-md mt-8">
+								REGISTER
+							</button>
+						</div>
+					</div>
+				</div>
+
+
+
 			</section>
 
 			{/* block mx-sz:w-[600px] mx-sz:h-[600px] */}
